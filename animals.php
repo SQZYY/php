8c4dd055f4ef6b0d65212ce8b95823c07bd3b807
Egor <egorax1le@yandex.ru>
2017-04-03 13:39:16 +0300

    3rd upd

--- animals.php
+++ animals.php
@@ -1,17 +1,17 @@
 <?php
 
-$animals = array('Panthera leo', 'Hippotigris', 'Tigris', 'Lupus', 'Castor canadensis', 'Grizzly', 
-				'Myrmecophaga tridactyla', 'Dasyprocta', 'Phascolarctos cinereus', 'Notoryctes', 
-				'Arctocephalus gazella', 'Procellariidae');
+$animals = array('Panthera leo', 'Hippotigris', 'Tigris', 'Lupus', 'Castor canadensis', 'Grizzly',
+    'Myrmecophaga tridactyla', 'Dasyprocta', 'Phascolarctos cinereus', 'Notoryctes',
+    'Arctocephalus gazella', 'Procellariidae');
 
-		
+
 $firstName = array('Pathera', 'Castor', 'Myrmecophaga', 'Phascolarctos', 'Arctocephalus');
 
 $secondName = array('leo', 'canadensis', 'tridactyla', 'cinereus', 'gazella');
 
-
-		$rand_keys = array_rand($firstName, 2);
-		echo $firstName[$rand_keys[0]] . "\n";
-
-		$rand_keys = array_rand($secondName, 2);
-		echo $secondName[$rand_keys[0]];
+foreach ($firstName as $name) {
+    $rand_keys = array_rand($firstName, 2);
+    echo $firstName[$rand_keys[0]] . "\n";
+    $rand_keys = array_rand($secondName, 2);
+    echo $secondName[$rand_keys[1]] . "\r" . "<br/>";
+}
