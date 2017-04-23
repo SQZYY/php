f0302ec94f6dddcd04d9f0160a26a82a9e206d72
Egor <egorax1le@yandex.ru>
2017-04-23 22:43:17 +0300

    5th upd

--- animals.php
+++ animals.php
@@ -1,18 +1,24 @@
 <?php
 
-//$animals = array('Panthera leo', 'Hippotigris', 'Tigris', 'Lupus', 'Castor canadensis', 'Grizzly',
-    //'Myrmecophaga tridactyla', 'Dasyprocta', 'Phascolarctos cinereus', 'Notoryctes',
-    //'Arctocephalus gazella', 'Procellariidae');
+$animals = array('Panthera leo', 'Hippotigris', 'Tigris', 'Lupus', 'Castor canadensis', 'Grizzly',
+    'Myrmecophaga tridactyla', 'Dasyprocta', 'Phascolarctos cinereus', 'Notoryctes',
+    'Arctocephalus gazella', 'Procellariidae');
 
-$fantasticAnimals = array('Panthera leo', 'Castor canadensis','Myrmecophaga tridactyla',
-    'Phascolarctos cinereus','Arctocephalus gazella');
-//$firstName = array('Pathera', 'Castor', 'Myrmecophaga', 'Phascolarctos', 'Arctocephalus');
+$firstName = [];
+$secondName = [];
 
-//$secondName = array('leo', 'canadensis', 'tridactyla', 'cinereus', 'gazella');
-
-foreach ($fantasticAnimals as $name) {
-    $rand_keys = array_rand($fantasticAnimals, 2);
-    echo $fantasticAnimals[$rand_keys[0]] . "\n";
-    $rand_keys = array_rand($fantasticAnimals, 2);
-    echo $fantasticAnimals[$rand_keys[1]] . "\r" . "<br/>";
-}
+    foreach ($animals as $animal)
+    {
+        $nameOfAnimal = str_word_count($animal, 1);
+            if (count($nameOfAnimal) > 1)
+            {
+                $firstName[] = array_shift($nameOfAnimal);
+                $secondName[] = array_pop($nameOfAnimal);
+            }
+    }
+        foreach ($firstName as $name) {
+        $rand_keys = array_rand($firstName, 2);
+        echo $firstName[$rand_keys[1]] . "\n";
+        $rand_keys = array_rand($secondName, 2);
+        echo $secondName[$rand_keys[0]] . "\r" . "<br/>";
+    }
