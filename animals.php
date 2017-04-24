2575caebdc08be4f0c90c97535b3d55fa1c7bcb1
Egor <egorax1le@yandex.ru>
2017-04-25 01:19:07 +0300

    6th upd

--- animals.php
+++ animals.php
@@ -1,24 +1,26 @@
 <?php
 
-$animals = array('Panthera leo', 'Hippotigris', 'Tigris', 'Lupus', 'Castor canadensis', 'Grizzly',
+ $animals = array('Panthera leo', 'Hippotigris', 'Tigris', 'Lupus', 'Castor canadensis', 'Grizzly',
     'Myrmecophaga tridactyla', 'Dasyprocta', 'Phascolarctos cinereus', 'Notoryctes',
     'Arctocephalus gazella', 'Procellariidae');
 
 $firstName = [];
 $secondName = [];
 
-    foreach ($animals as $animal)
-    {
+    foreach ($animals as $animal) {
         $nameOfAnimal = str_word_count($animal, 1);
             if (count($nameOfAnimal) > 1)
             {
-                $firstName[] = array_shift($nameOfAnimal);
-                $secondName[] = array_pop($nameOfAnimal);
+                $firstName[] = $nameOfAnimal[0];
+                $secondName[] = $nameOfAnimal[1];
             }
     }
-        foreach ($firstName as $name) {
-        $rand_keys = array_rand($firstName, 2);
-        echo $firstName[$rand_keys[1]] . "\n";
-        $rand_keys = array_rand($secondName, 2);
-        echo $secondName[$rand_keys[0]] . "\r" . "<br/>";
-    }
+
+shuffle($firstName);
+shuffle($secondName);
+
+for ($i = 0; $i <= count($firstName); $i++)
+{
+    echo $firstName[$i] . ' ' . $secondName[$i];
+    echo '<br>';
+}
