28d401de11880a94fd144d700a798f7ff65390ba
Egor <egorax1le@yandex.ru>
2017-05-17 02:29:25 +0300

    last upd

--- secondsql.php
+++ secondsql.php
@@ -3,20 +3,36 @@
 $pdo = new PDO("mysql:host=localhost;dbname=homework;charset=utf8", "root", "1234", [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
-if ($_POST) {
+
+if (isset($_POST['description']) ? $_POST['description'] : '') {
     $desc = $_POST['description'];
-    $add = "INSERT INTO tasks (description, date_added) VALUES ('$desc', CURRENT_TIME)";
+    $add = "INSERT INTO tasks (description, date_added) VALUES (:description, CURRENT_TIME)";
     $statementTwo = $pdo->prepare($add);
+    $statementTwo->bindValue(':description', $desc);
     $statementTwo->execute();
     header("location:/secondsql.php");
 }
-if ($_GET)  {
+
+if (isset($_GET['id']) ? $_GET['id'] : '') {
     $id = $_GET['id'];
-    $delete = "DELETE FROM tasks WHERE id = $id";
-    $statementThree = $pdo->prepare($delete);
-    $statementThree->execute();
-    header("location:/secondsql.php");
+    if ('&action=done') {
+        $done = "UPDATE tasks SET is_done = 1 WHERE id = $id";
+        $statementFour = $pdo->prepare($done);
+        $statementFour->execute();
+        header("location:/secondsql.php");
+    }
+    if ('&action=delete') {
+        $delete = "DELETE FROM tasks WHERE id = $id";
+        $statementThree = $pdo->prepare($delete);
+        $statementThree->execute();
+        header("location:/secondsql.php");
+    }
 }
+
+$sql = "SELECT * FROM tasks";
+$statementOne = $pdo->prepare($sql);
+$statementOne->execute();
+
 ?>
 
 <html>
@@ -34,8 +50,15 @@ if ($_GET)  {
     table th {
         background: #eee;
     }
-</style>
 
+    span.orange {
+        color: orange;
+    }
+
+    span.green {
+        color: green;
+    }
+</style>
 <h1>Список дел на сегодня</h1>
 <div style="float: left">
     <form method="POST">
@@ -44,7 +67,6 @@ if ($_GET)  {
     </form>
 </div>
 <div style="clear: both"></div>
-
 <table>
     <tr>
         <th>Описание задачи</th>
@@ -52,21 +74,17 @@ if ($_GET)  {
         <th>Статус</th>
         <th></th>
     </tr>
-<?php
-    $sql = "SELECT * FROM tasks";
-    $statementOne = $pdo->prepare($sql);
-    $statementOne->execute();
-?>
     <?php foreach ($statementOne as $row) { ?>
     <tr>
         <td><?= htmlspecialchars($row["description"], ENT_QUOTES);?></td>
         <td><?= htmlspecialchars($row["date_added"], ENT_QUOTES);?></td>
-        <td><?= htmlspecialchars($row["is_done"], ENT_QUOTES);?></td>
+        <td><?php if ($row["is_done"] == 0) { echo '<span class="orange">В процессе</span>';
+            } else { echo '<span class="green">Выполнено</span>';} ?></td>
         <td>
-            <a href='?id=<?php echo $row['id'] ?>&action=delete'>Удалить</a>
+            <a href='?id=<?= $row["id"] ?>&action=done'>Выполнить</a>
+            <a href='?id=<?= $row["id"] ?>&action=delete'>Удалить</a>
         </td>
     </tr>
     <?php } ?>
 </table>
-
 </html>
